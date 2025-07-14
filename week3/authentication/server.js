0635fb1796a2b4a0eb109bba9938a991e0de01aa
arnav30kulkarni <arnav30kulkarni@gmail.com>
2025-07-14 21:01:56 +0530

    Add files via upload

---
+++ week3/authentication/server.js
@@ -0,0 +1,39 @@
+// AUTHENTICATION USING MIDDLEWARES
+
+const express = require("express");
+const app = express();
+
+// // middlewares are used to authenticate than to just keep repeating same logic over and over again
+function userMiddleware(req,res,next){
+    
+    const password = req.headers.password;
+    const username = req.headers.username;
+
+    if(username != "arnav" || password != "password"){
+        res.status(403).json({msg:"incorrect inputs please check again :( "})
+    }else{
+        next()
+    }
+}
+// // middleware 2
+function kidneyMiddleware(req,res,next){
+    const kidneyId = req.query.kidneyId;
+    if(kidneyId !=1 && kidneyId != 2){
+        res.status(403).json({msg:"incorrect inputs please check again :("})
+    }else{
+        next()
+    }
+    }
+
+// get request
+app.get("/healthCheckUp", userMiddleware , kidneyMiddleware, (req,res)=>{
+    res.json({msg:"your health is good :)"})
+})
+
+app.get("/heartCheckUp", userMiddleware,(req,res)=>{
+    res.json({msg:"your heart is perfectly good :)"})
+})
+
+app.listen(3000,()=>{
+    console.log("listening at port 3000.")
+})
